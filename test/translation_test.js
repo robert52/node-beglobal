a689e45d6e07b4de8230effb17c1fe12c39d7c6e
Robert Onodi <usdh_1989@yahoo.com>
2013-10-07 18:09:18 +0300

    removed console log

--- test/translation_test.js
+++ test/translation_test.js
@@ -7,7 +7,6 @@ var beglobal = new BeglobalAPI(configs);
 describe('Translations', function() {
   it('should get a translation', function(done) {
     beglobal.translations.translate({text: 'Hello', from: 'eng', to: 'fra'}, function(err, results) {
-      console.log(err, results);
       should.not.exist(err);
       should.exist(results);
       done();
